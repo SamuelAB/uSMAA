0835f7eb99669bcac420de191af68e7b7c801bf7
Thomas Hourdel <chman.a11w@gmail.com>
2014-05-01 12:11:41 +0200

    Removed ApplyEffect, the same can be achieved by simply enabling/disabling the component

--- SMAA/SMAA.cs
+++ SMAA/SMAA.cs
@@ -2,7 +2,6 @@
 
 public class SMAA : MonoBehaviour
 {
-	public bool ApplyEffect;
 	public int State = 1;
 	public int Passes = 1;
 
@@ -31,59 +30,52 @@ public class SMAA : MonoBehaviour
 
 		Vector4 metrics = new Vector4(1 / (float)Screen.width, 1 / (float)Screen.height, Screen.width, Screen.height);
 
-		if (this.ApplyEffect)
+		if (State == 1)
 		{
-			if (State == 1)
-			{
-				Graphics.Blit(source, destination, mat, 0);
-			}
-			else if (State == 2)
-			{
-				mat.SetTexture("areaTex", GameObject.Find("TextureGenerator").GetComponent<AreaTexture>().alphaTex);
-				mat.SetTexture("luminTex", GameObject.Find("TextureGenerator").GetComponent<AreaTexture>().luminTex);
-				mat.SetTexture("searchTex", GameObject.Find("TextureGenerator").GetComponent<SearchTexture>().alphaTex);
-				mat.SetVector("SMAA_RT_METRICS", metrics);
-
-				var rt = RenderTexture.GetTemporary(Screen.width, Screen.height, 0);
-
-				Graphics.Blit(source, rt, mat, 0);
-				Graphics.Blit(rt, destination, mat, 1);
-
-				rt.Release();
-			}
-			else if (State == 3)
-			{
-				mat.SetTexture("areaTex", GameObject.Find("TextureGenerator").GetComponent<AreaTexture>().alphaTex);
-				mat.SetTexture("luminTex", GameObject.Find("TextureGenerator").GetComponent<AreaTexture>().luminTex);
-				mat.SetTexture("searchTex", GameObject.Find("TextureGenerator").GetComponent<SearchTexture>().alphaTex);
-				mat.SetTexture("_SrcTex", source);
-				mat.SetVector("SMAA_RT_METRICS", metrics);
-
-				var rt = RenderTexture.GetTemporary(Screen.width, Screen.height, 0);
-				var rt2 = RenderTexture.GetTemporary(Screen.width, Screen.height, 0);
-				var rt3 = RenderTexture.GetTemporary(Screen.width, Screen.height, 0);
-
-				Graphics.Blit(source, rt3);
-				for (var i = 0; i < Passes; i++)
-				{
-					Graphics.Blit(black, rt);
-					Graphics.Blit(black, rt2);
-
-					Graphics.Blit(rt3, rt, mat, 0);
-
-					Graphics.Blit(rt, rt2, mat, 1);
-					Graphics.Blit(rt2, rt3, mat, 2);
-				}
-				Graphics.Blit(rt3, destination);
-
-				rt.Release();
-				rt2.Release();
-				rt3.Release();
-			}		
+			Graphics.Blit(source, destination, mat, 0);
 		}
-		else
+		else if (State == 2)
 		{
-			Graphics.Blit(source, destination);
+			mat.SetTexture("areaTex", GameObject.Find("TextureGenerator").GetComponent<AreaTexture>().alphaTex);
+			mat.SetTexture("luminTex", GameObject.Find("TextureGenerator").GetComponent<AreaTexture>().luminTex);
+			mat.SetTexture("searchTex", GameObject.Find("TextureGenerator").GetComponent<SearchTexture>().alphaTex);
+			mat.SetVector("SMAA_RT_METRICS", metrics);
+
+			var rt = RenderTexture.GetTemporary(Screen.width, Screen.height, 0);
+
+			Graphics.Blit(source, rt, mat, 0);
+			Graphics.Blit(rt, destination, mat, 1);
+
+			rt.Release();
+		}
+		else if (State == 3)
+		{
+			mat.SetTexture("areaTex", GameObject.Find("TextureGenerator").GetComponent<AreaTexture>().alphaTex);
+			mat.SetTexture("luminTex", GameObject.Find("TextureGenerator").GetComponent<AreaTexture>().luminTex);
+			mat.SetTexture("searchTex", GameObject.Find("TextureGenerator").GetComponent<SearchTexture>().alphaTex);
+			mat.SetTexture("_SrcTex", source);
+			mat.SetVector("SMAA_RT_METRICS", metrics);
+
+			var rt = RenderTexture.GetTemporary(Screen.width, Screen.height, 0);
+			var rt2 = RenderTexture.GetTemporary(Screen.width, Screen.height, 0);
+			var rt3 = RenderTexture.GetTemporary(Screen.width, Screen.height, 0);
+
+			Graphics.Blit(source, rt3);
+			for (var i = 0; i < Passes; i++)
+			{
+				Graphics.Blit(black, rt);
+				Graphics.Blit(black, rt2);
+
+				Graphics.Blit(rt3, rt, mat, 0);
+
+				Graphics.Blit(rt, rt2, mat, 1);
+				Graphics.Blit(rt2, rt3, mat, 2);
+			}
+			Graphics.Blit(rt3, destination);
+
+			rt.Release();
+			rt2.Release();
+			rt3.Release();
 		}
 	}
 }
